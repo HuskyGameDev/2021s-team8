d576f12d64c586184b2ab2def15a175307509d71
NoahSKol <noahkolcz@gmail.com>
2021-03-25 13:02:18 -0400

    Implemented spikes that pop out and retract on a cycle
    Created a simple script that enables and disables spikes on a cycle. The cycle length determines how long, in seconds, the cycle takes.

---
+++ Jewel Runner/Assets/CycleSpikesScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CycleSpikesScript : MonoBehaviour
+{
+    private bool isOut = false;
+    [SerializeField] private float cycleLength;
+
+
+    void Start()
+    {
+        InvokeRepeating("Cycle", 0, cycleLength);
+    }
+
+    public void Cycle()
+    {
+        if (isOut)
+        {
+            GetComponent<BoxCollider2D>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            isOut = false;
+        } else
+        {
+            GetComponent<BoxCollider2D>().enabled = true;
+            GetComponent<SpriteRenderer>().enabled = true;
+            isOut = true;
+        }
+    }
+}
