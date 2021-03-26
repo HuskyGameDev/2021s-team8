46cf73632294de7630751859ccb4ae388edf5f9a
riverDallas134 <61401928+riverDallas134@users.noreply.github.com>
2021-03-25 22:59:14 -0400

    Added Pause Screen

---
+++ Jewel Runner/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool Paused = false;
+    public GameObject pauseMenu;
+
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Paused)
+            {
+                Resume();
+            }else
+            {
+                Pause();
+            }
+        }
+
+
+        void Start ()
+            {
+                pauseMenu.SetActive(false);
+            }
+
+        void Resume()
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1f;
+            Paused = false;
+        }
+
+        void Pause()
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0f;
+            Paused = true;
+        }
+    }
+}
