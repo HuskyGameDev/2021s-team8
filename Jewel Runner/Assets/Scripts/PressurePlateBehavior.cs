c9e8e3d071ad224b33709d11e3b9c47c1ddea50c
NoahSKol <noahkolcz@gmail.com>
2021-04-15 20:05:56 -0400

    Added a spike wall and pressure plate
    The spike wall moves in the direction that the spikes are facing once the pressure plate is pressed. Also modified the pressure plate material to render it properly as pixels instead of looking fuzzy

---
+++ Jewel Runner/Assets/Scripts/PressurePlateBehavior.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlateBehavior : MonoBehaviour
+{
+    private SpriteRenderer SpriteRenderer; //the pressure plate's sprite renderer
+    private BoxCollider2D BoxCollider2D; //the pressure plate's box collider
+    private bool IsPressed; //whether or not the pressure plate has been pressed
+
+    void Start() //initializes values
+    {
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        BoxCollider2D = GetComponent<BoxCollider2D>();
+        IsPressed = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) //when the player steps on the pressure plate, the pressure plate disapears and becomes pressed
+    {
+        if(other.tag == "Player")
+        {
+            IsPressed = true;
+            SpriteRenderer.enabled = false;
+            BoxCollider2D.enabled = false;
+        }
+    }
+
+    public bool GetIsPressed()
+    {
+        return IsPressed;
+    }
+}
---
+++ Jewel Runner/Assets/SpikeWallBehavior.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeWallBehavior : MonoBehaviour
+{
+    [SerializeField] private PressurePlateBehavior PressurePlate; //script of pressure plate that activates the spike wall
+    [SerializeField] [Range(1,20)] private float MoveAmount = 1; //distance the spike wall will travel
+    [SerializeField] [Range(1,10)] private float MoveSpeed = 1; //speed the spike wall travels at
+    private float MoveDistance; //the distance the spike wall has traveled
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MoveDistance = 0f; //makes sure every time the scene is loaded, the distance traveled is set to zero
+    }
+
+    void Update()
+    {
+        if (PressurePlate.GetIsPressed() && MoveDistance < MoveAmount) //if the pressure plate is pressed and the spike wall hasn't reached its final destination, the spikewall moves
+        {
+            transform.Translate(0, MoveSpeed * MoveAmount * Time.deltaTime, 0);
+            MoveDistance += MoveSpeed * Time.deltaTime;
+        }
+    }
+}
