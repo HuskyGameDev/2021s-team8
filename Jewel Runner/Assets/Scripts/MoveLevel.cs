6c4df9aafcc8c9a7ca257f3ce5633f107c3dceba
riverDallas134 <61401928+riverDallas134@users.noreply.github.com>
2021-04-18 23:45:17 -0400

    Added Level Progression Script/more

---
+++ Jewel Runner/Assets/Scripts/MoveLevel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MoveLevel : MonoBehaviour
+{
+    public GameObject player;       //player
+    private bool door = false;     //checks if we are touching the door
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        door = true;       //checks if player is touching door
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        door = false;      // checks if player is touching door
+    }
+
+    IEnumerator DelayedStart()
+        {
+            yield return new WaitForSeconds(.01f);
+            SceneManager.LoadScene("Simone"); //Loads the scene based off of button input
+        }
+
+    void Update()
+    {
+        // loads new level if e is pressed and touching door
+        if (Input.GetKeyDown(KeyCode.E) && door)
+        {
+            DelayedStart();                 // sets new level
+            door = false;     // checks if player is touching door
+        }
+    }
+}
