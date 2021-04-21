e556c2f97331b8379773068529a15fa68cc339a6
Simone Tilbury <sytilbur@mtu.edu>
2021-04-21 18:54:29 -0400

    Added Ending Scene and Credits

---
+++ Jewel Runner/Assets/Scripts/EndSeq.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndSeq : MonoBehaviour
+{
+    private bool touching = false;
+    public Animator animator;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Fade();
+        }
+    }
+
+    public void Fade()
+    {
+        animator.SetTrigger("Fade");
+    }
+
+    public void onFadeComp()
+    {
+        SceneManager.LoadScene("End1");
+    }
+
+
+}
---
+++ Jewel Runner/Assets/Scripts/WaitTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WaitTime : MonoBehaviour
+{
+    private float delay = 3f;
+    private float timeElapsed;
+    public Animator animator;
+    public string sceneName;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if(timeElapsed > delay)
+        {
+            Fade();
+        }
+    }
+
+    public void Fade()
+    {
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void onFadeComp()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
