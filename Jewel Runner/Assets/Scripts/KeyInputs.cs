01fc5580fadb621d5168412950e4ebd5189f2163
riverDallas134 <61401928+riverDallas134@users.noreply.github.com>
2021-11-04 23:39:19 -0400

    River Dallas
    added new level, completed river level 2, and also worked on being able to remap keys.

---
+++ Jewel Runner/Assets/Scripts/KeyInputs.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInputs : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultKeys = new Dictionary<string, KeyCode>();
+
+
+    }
+
+    Dictionary<string, KeyCode> defaultKeys;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public bool GetButtonDown( string buttonName )
+    {
+
+        if( defaultKeys.ContainsKey(buttonName) == false)
+        {
+        return false;
+
+        }
+        return false;
+    }
+
+    public void SetButtonForKey( string buttonName, KeyCode keyCode )
+        {
+            defaultKeys[buttonName] = keyCode;
+        }
+
+}
